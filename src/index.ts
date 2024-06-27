06bcc45b1d9eba436be110c64c9ba281c13e0bf6
ifan3735 <ifanmaina@gmail.com>
2024-06-27 17:28:41 +0300

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { bookRouter } from './books/books.router'
+import { cors } from 'hono/cors'
 import 'dotenv/config'
 const app = new Hono()
+
+app.use('*', cors({
+  origin: 'http://localhost:5173',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
